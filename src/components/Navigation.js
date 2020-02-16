d9e8d9a3fe824d2d80d85a7146c80ad8cf4901b5
Arek Banaś <arek.banas@hotmail.com>
2020-02-16 21:28:40 +0100

    changes for mobile device

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,8 +16,8 @@ const Navigation = (props) => {
             <div className={props.active ? 'nav-container active' : 'nav-container'}>
                 <div className="main-nav">
                     <div className={props.active ? "main-nav__select-lng active" : "main-nav__select-lng"}>
-                        <i className="fa fa-language"></i>
-                        <select onClick={props.hide}>
+                        <i className="fa fa-angle-down"></i>
+                        <select onChange={props.hide}>
                             <option className="option1" onClick={() => i18n.changeLanguage('pl')}>PL</option>
                             <option className="option2" onClick={() => i18n.changeLanguage('en')}>EN</option>
                         </select>
